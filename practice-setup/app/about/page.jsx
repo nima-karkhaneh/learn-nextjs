bbf8ac7c945369b30ff22795ddaa29f6f70cae8e
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-12-04 21:30:58 +1100

    Add gitignore and a practice nextjs project

---
+++ practice-setup/app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function SubHeading() {
+    return <h2>This course is delivered by Bob Ziroll</h2>
+}
---
+++ practice-setup/app/products/page.jsx
@@ -0,0 +1,8 @@
+
+function Products() {
+  return (
+    <h2>This is the products page.</h2>
+  )
+}
+
+export default Products
