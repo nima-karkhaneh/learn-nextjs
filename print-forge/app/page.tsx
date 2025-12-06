12c333d550bf38450b50452b45c443ab00fbab20
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-12-06 18:11:36 +1100

    Add home page for print-forge project

--- print-forge/app/page.tsx
+++ print-forge/app/page.tsx
@@ -1,4 +1,14 @@
 import Image from "next/image";
 export default function Home() {
-  return
+  return (
+    <main className="home-cont">
+        <aside className="home-info-cont">
+          <p className="top-txt">Your go-to platform for 3D printing files</p>
+          <h1>Discover what’s possible with 3D printing</h1>
+          <p className="btm-txt">Join our community of creators and explore a vast library of user-submitted models.</p>
+          <button>Browse models</button>
+        </aside>
+        <img src="/hero-image.png" alt="" />
+    </main>
+  )
 }
