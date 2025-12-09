89ded996750173fd8a60ba62714ec03b915fefa5
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-12-09 13:28:59 +1100

    Load google fonts in layout.tsx

--- print-forge/app/layout.tsx
+++ print-forge/app/layout.tsx
@@ -1,5 +1,20 @@
 
+import { subscribe } from "diagnostics_channel";
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
+
+
+const alberSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display:"swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates"
+})
 
 
 export default function RootLayout({
@@ -9,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${alberSans.className} ${montserratAlternates.variable}`}>
         <header>
           <div className="logo">
             <img src="/printforge-logo.svg" alt="logo" />
