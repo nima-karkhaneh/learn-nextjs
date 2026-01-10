4e3ecb421e921dfcd0b92b542dc44a88843ee620
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2026-01-10 19:47:48 +1100

    Remove extra <li></li> from Navbar.tsx

--- print-forge/app/components/Navbar.tsx
+++ print-forge/app/components/Navbar.tsx
@@ -24,12 +24,8 @@ export default function Navbar() {
           </div>
         </Link>
         <ul className="flex items-center gap-2.5">
-          <li className="text-sm uppercase cursor-pointer">
             <NavLink href="/3d-models">3D Models</NavLink>
-          </li>
-          <li className="text-sm uppercase cursor-pointer">
             <NavLink href="/about">About</NavLink>
-          </li>
         </ul>
       </nav>
     </header>
