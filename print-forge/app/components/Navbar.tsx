0597aa02204bb2d42314a1a1f035d32705c0b70a
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2026-01-12 18:54:51 +1100

    Display the active link for the side navbar

--- print-forge/app/components/Navbar.tsx
+++ print-forge/app/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
           </div>
         </Link>
         <ul className="flex items-center gap-2.5">
-            <NavLink href="/3d-models" isActive={path === "/3d-models"}>3D Models</NavLink>
+            <NavLink href="/3d-models" isActive={path.startsWith("/3d-models")}>3D Models</NavLink>
             <NavLink href="/about" isActive={path === "/about"}>About</NavLink>
         </ul>
       </nav>
